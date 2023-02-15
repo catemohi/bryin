e1e400f7f4df6658243c1512b644fce59e0eb721
catemohi <catemohi@gmail.com>
2023-02-16 01:13:47 +0300

    начало написания фронта

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bryin.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bryin.default-settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
